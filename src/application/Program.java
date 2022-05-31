2261634c3d7705525978d66334d8c366f4a047d0
mucarb <murilo.rb60@gmail.com>
2022-05-31 13:38:06 -0300

    Iniciado a implementacao de Tabuleiro e Peca

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,12 @@
 package application;
 
-import jogotabuleiro.Posicao;
+import jogotabuleiro.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Posicao posicao = new Posicao(3, 5);
-		
-		System.err.println(posicao);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 	}
 
 }
